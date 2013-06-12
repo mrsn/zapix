5fa9161377a1ae63adb3b937746c20b84bb39216
stoyan <stoyanov@adesso-mobile.de>
2013-06-12 10:24:43 +0200

    added initial version ot the gem

--- lib/zapix.rb
+++ lib/zapix.rb
@@ -1,5 +1,29 @@
 require "zapix/version"
+require_relative "zapix/zabbix_rpc_client"
 
-module Zapix
-  # Your code goes here...
+class ZabbixAPI
+  attr :client
+
+  def self.connect(options = {})
+    new(options)
+  end
+
+  def initialize(options = {})
+    @client = ZabbixRPCClient.new(options)
+    Dir["#{File.dirname(__FILE__)}/zabbixapi/zabbix_classes/*.rb"].each { |f| load(f) }
+    Dir["#{File.dirname(__FILE__)}/zabbixapi/proxies/*.rb"].each { |f| load(f) }
+  end
+
+  def hostgroups
+    @hostgroups ||= HostGroups.new(@client)
+  end
+
+  def hosts
+    @hosts ||= Hosts.new(@client)
+  end
+
+  def templates
+    @templates ||= Templates.new(@client)
+  end
+
 end
