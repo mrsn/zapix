928beb7313067fa2c06c7a1a72a2898ae4ea88c7
stoyan <stoyanov@adesso-mobile.de>
2013-06-17 10:48:29 +0200

    interface class now uses active record validations

--- lib/zapix/zabbix_classes/interface.rb
+++ lib/zapix/zabbix_classes/interface.rb
@@ -1,15 +1,39 @@
+require "active_record"
+
 class Interface
+  include ActiveModel::Validations
 
-  attr_reader :properties
+  attr_reader :type, :main, :useip, :ip, :dns, :port
+  validates_presence_of :type, :main, :useip, :ip, :dns, :port
 
-  def initialize(options)
-    @properties = {}
-    @properties["type"] = options["type"] ||= 1
-    @properties["main"] = options["main"] ||= 1
-    @properties["useip"] = options["useip"] ||= 1
-    @properties["ip"] = options["ip"]
-    @properties["dns"] = options["dns"]
-    @properties["port"] = options["port"] ||= 10050
+  # for more info see
+  #  https://www.zabbix.com/documentation/2.0/manual/appendix/api/hostinterface/definitions#host_interface
+  # we assume ip and dns shall always be set
+  validates_inclusion_of :type, :in => 1..4
+  validates_inclusion_of :main, :in => 0..1
+  validates_inclusion_of :useip, :in => 0..1
+
+  def initialize(attributes)
+    @type = attributes["type"] ||= 1
+    @main = attributes["main"] ||= 1
+    @useip = attributes["useip"] ||= 1
+    @ip = attributes["ip"] = attributes["ip"]
+    @dns = attributes["dns"] = attributes["dns"]
+    @port = attributes["port"] = attributes["port"] ||= 10050
+    @result = { "type" => type,
+                "main" => main,
+                "useip" => useip,
+                "ip" => ip,
+                "dns" => dns,
+                "port" => port }
+  end
+
+  def read_attribute_for_validation(key)
+    send(key)
+  end
+
+  def to_hash
+    @result
   end
 
 end
