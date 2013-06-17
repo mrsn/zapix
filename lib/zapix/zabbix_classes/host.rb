75eb8390313623e085e34746bd27d3c6d71e002d
stoyan <stoyanov@adesso-mobile.de>
2013-06-17 11:25:58 +0200

    refactored host class

--- lib/zapix/zabbix_classes/host.rb
+++ lib/zapix/zabbix_classes/host.rb
@@ -1,8 +1,8 @@
 class Host
 
-  attr_accessor :properties, :group_ids, :template_ids, :interfaces 
+  attr_accessor :group_ids, :template_ids, :interfaces 
 
-  def initialize
+  def initialize()
     @group_ids = Array.new
     @template_ids = Array.new
     @interfaces = Array.new
@@ -10,26 +10,32 @@ class Host
   end
 
   def add_name(name)
-    properties.merge!("host" => name)
+    @properties.merge!("host" => name)
   end
 
   def add_group_ids(*ids)
     ids.each do |id|
       group_ids << {"groupid" => id}
     end
-    properties.merge!("groups" => group_ids)
+    @properties.merge!("groups" => group_ids)
   end
 
-  def add_interface(interface)
-    interfaces << interface
-    properties.merge!("interfaces" => interfaces)
+  def add_interface(*ifaces)
+    interfaces.concat(ifaces)
+    @properties.merge!("interfaces" => interfaces)
   end
 
   def add_template_ids(*ids)
     ids.each do |id|
       template_ids << {"templateid" => id}
     end
-    properties.merge!("templates" => template_ids)
+    @properties.merge!("templates" => template_ids)
+  end
+
+  def to_hash
+    @properties
   end
 
 end
+
+
